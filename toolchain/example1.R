3cf150884941e7c85e752cc6078729455e18e7e0
dreibh <dreibh@6f377267-d4fd-0310-adf2-e6e4f864fedf>
2011-11-22 13:00:09 +0000

    Some fixes.
    git-svn-id: file:///home/dreibh/tmp/essen-omnetpp/trunk@1011 6f377267-d4fd-0310-adf2-e6e4f864fedf

---
+++ toolchain/example1.R
@@ -0,0 +1,36 @@
+# ###########################################################################
+# Name:        example1
+# Description:
+# Revision:    $Id$
+# ###########################################################################
+
+source("simulate-version14.R")
+
+
+# ====== Simulation Settings ================================================
+simulationDirectory <- "example1"
+simulationRuns <- 3
+simulationDuration <- 120
+simulationStoreVectors <- FALSE
+simulationExecuteMake <- TRUE
+simulationScriptOutputVerbosity <- 3
+simulationSummaryCompressionLevel <- 9
+simulationSummarySkipList <- c("lan.")
+# -------------------------------------
+source("computation-pool.R")
+# -------------------------------------
+
+# ###########################################################################
+
+simulationConfigurations <- list(
+   list("puToPERatio", 1, 2, 3, 5, 7, 10, 15, 20),
+
+   list("scenarioNumberOfCalcAppPoolElementsVariable", 10),
+
+   list("calcAppPoolElementSelectionPolicy", "LeastUsed", "Random", "RoundRobin"),
+   list("calcAppPoolUserServiceJobSizeVariable", 1e6, 1e8)
+)
+
+# ###########################################################################
+
+createSimulation(simulationDirectory, simulationConfigurations, rspsim5DefaultConfiguration)
---
+++ toolchain/plot-example1.R
@@ -0,0 +1,78 @@
+# ###########################################################################
+# Name:        example1
+# Description:
+# Revision:    $Id$
+# ###########################################################################
+
+source("simulate-version14.R")
+
+
+# ------ Plotter Settings ---------------------------------------------------
+simulationDirectory  <- "example1"
+plotColorMode        <- cmColor
+plotHideLegend       <- FALSE
+plotLegendSizeFactor <- 0.8
+plotOwnOutput        <- FALSE
+plotFontFamily       <- "Helvetica"
+plotFontPointsize    <- 22
+plotWidth            <- 10
+plotHeight           <- 10
+plotConfidence       <- 0.95
+
+# ###########################################################################
+
+# ------ Plots --------------------------------------------------------------
+plotConfigurations <- list(
+   # ------ Format example --------------------------------------------------
+   # list(simulationDirectory, "output.pdf",
+   #      "Plot Title",
+   #      list(xAxisTicks) or NA, list(yAxisTicks) or NA, list(legendPos) or NA,
+   #      "x-Axis Variable", "y-Axis Variable",
+   #      "z-Axis Variable", "v-Axis Variable", "w-Axis Variable",
+   #      "a-Axis Variable", "b-Axis Variable", "p-Axis Variable")
+   # ------------------------------------------------------------------------
+
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-Utilization.pdf"),
+        "Provider's Perspective", NA, NA, list(1,0),
+        "puToPERatio", "controller.SystemAverageUtilization",
+        "calcAppPoolElementSelectionPolicy", "jsToSC", ""),
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-HandlingSpeed.pdf"),
+        "User's Perspective", NA, NA, list(0,1),
+        "puToPERatio", "controller.SystemAverageHandlingSpeed",
+        "calcAppPoolElementSelectionPolicy", "jsToSC", ""),
+
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-ASAPPackets.pdf"),
+        "Provider's Perspective", NA, NA, list(1,1),
+        "puToPERatio", "controller.TransportNodeGlobalASAPPackets",
+        "calcAppPoolElementSelectionPolicy", "jsToSC", ""),
+   list(simulationDirectory, paste(sep="", simulationDirectory, "-ENRPPackets.pdf"),
+        "Provider's Perspective", NA, NA, list(1,1),
+        "puToPERatio", "controller.TransportNodeGlobalENRPPackets",
+        "calcAppPoolElementSelectionPolicy", "jsToSC", "")
+)
+
+
+# ------ Variable templates -------------------------------------------------
+plotVariables <- append(list(
+   # ------ Format example --------------------------------------------------
+   # list("Variable",
+   #         "Unit[x]{v]"
+   #          "100.0 * data1$x / data1$y", <- Manipulator expression:
+   #                                           "data" is the data table
+   #                                        NA here means: use data1$Variable.
+   #          "myColor",
+   #          list("InputFile1", "InputFile2", ...))
+   #             (simulationDirectory/Results/....data.tar.bz2 is added!)
+   # ------------------------------------------------------------------------
+
+   # list("controller.SystemAverageUtilization",
+   #         "Average Utilization[%]",
+   #         "100.0 * data1$controller.SystemAverageUtilization",
+   #         "blue4",
+   #         list("controller-SystemAverageUtilization"))
+
+), rspsim5PlotVariables)
+
+# ###########################################################################
+
+createPlots(simulationDirectory, plotConfigurations)
