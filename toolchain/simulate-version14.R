163b19fe15bcda4f3ac60020757ecb83ec286aff
Thomas Dreibholz <dreibh@simula.no>
2021-12-20 21:01:20 +0100

    Improved script.

--- toolchain/simulate-version14.R
+++ toolchain/simulate-version14.R
@@ -282,17 +282,19 @@ rspsim5WriteParameterSection <- function(filePrefix, iniFile, simulationRun, dur
       }
 
       # ------ Attackers ---------------------------------------------------
-      for(j in seq(1, as.numeric(Attackers[i]))) {
-         result <- eval(call(scenarioNetworkLANDelayDistribution, i, scenarioNumberOfLANs, "PU", j, as.numeric(Attackers[i]), scenarioNetworkLANDelayVariable, scenarioNetworkLANDelayGamma, scenarioNetworkLANDelayLambda))
-         lanDelay <- as.numeric(result[2])
-         # ------ Component address -----------------------------------------
-         cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].transportNode.interfaceAddress = ", nextComponentAddress, "\n", file=iniFile)
-         nextComponentAddress <- nextComponentAddress + 1
-         # ------ Registrar list --------------------------------------------
-         cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].attackerProcess.attackTargetRegistrarsList = \"", attackerStaticRegistrarList, "\"\n", file=iniFile)
-         # ------ Component link delay --------------------------------------
-         cat(sep="", "gammaScenario.lan[", i - 1, "].switch.toAttacker[", j - 1, "].channel.delay = ", lanDelay / 2, "ms\n", file=iniFile)
-         cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].toNetwork.channel.delay = ", lanDelay / 2, "ms\n", file=iniFile)
+      if(as.numeric(Attackers[i]) > 0) {
+         for(j in seq(1, as.numeric(Attackers[i]))) {
+            result <- eval(call(scenarioNetworkLANDelayDistribution, i, scenarioNumberOfLANs, "PU", j, as.numeric(Attackers[i]), scenarioNetworkLANDelayVariable, scenarioNetworkLANDelayGamma, scenarioNetworkLANDelayLambda))
+            lanDelay <- as.numeric(result[2])
+            # ------ Component address -----------------------------------------
+            cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].transportNode.interfaceAddress = ", nextComponentAddress, "\n", file=iniFile)
+            nextComponentAddress <- nextComponentAddress + 1
+            # ------ Registrar list --------------------------------------------
+            cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].attackerProcess.attackTargetRegistrarsList = \"", attackerStaticRegistrarList, "\"\n", file=iniFile)
+            # ------ Component link delay --------------------------------------
+            cat(sep="", "gammaScenario.lan[", i - 1, "].switch.toAttacker[", j - 1, "].channel.delay = ", lanDelay / 2, "ms\n", file=iniFile)
+            cat(sep="", "gammaScenario.lan[", i - 1, "].attackerArray[", j - 1, "].toNetwork.channel.delay = ", lanDelay / 2, "ms\n", file=iniFile)
+         }
       }
 
       # ------ Switch address -----------------------------------------------
