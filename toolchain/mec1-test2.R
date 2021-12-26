decc3585da724bddb88b33c95a3a9ace4f745227
Thomas Dreibholz <dreibh@simula.no>
2021-12-26 20:45:42 +0100

    Added scripts.

---
+++ toolchain/mec1-test2.R
@@ -0,0 +1,199 @@
+# ###########################################################################
+# Name:        example1
+# Description:
+# ###########################################################################
+
+source("simulate-version14.R")
+
+
+# ====== Simulation Settings ================================================
+simulationDirectory <- "mec1-test2"
+simulationRuns <- 3
+simulationDuration <- 120
+simulationStoreVectors <- FALSE
+simulationExecuteMake <- TRUE
+simulationScriptOutputVerbosity <- 3
+simulationSummaryCompressionLevel <- 9
+simulationSummarySkipList <- c()
+# -------------------------------------
+source("computation-pool.R")
+# -------------------------------------
+
+
+# ###### Number of PEs ######################################################
+mecCalcAppPoolElementsDistribution <- function(currentBlock, totalBlocks,
+                                               variable, gamma, lambda)
+{
+   if((currentBlock < 1) || (currentBlock > totalBlocks) ||
+      (variable < 0.0)) {
+      stop("mecCalcAppPoolElementsDistribution: Check parameters!")
+   }
+   if(totalBlocks != 3) {
+      stop("mecCalcAppPoolElementsDistribution: Invalid setting of totalBlocks!")
+   }
+
+   if(currentBlock == 1) {   # Local: As many PEs as PUs!
+
+
+#     !!!!!!!!!!!!!!!!!!!!!
+#       return(c("mecCalcAppPoolElementsDistribution", 1, 1))
+#     !!!!!!!!!!!!!!!!!!!!!
+
+      return(c("mecCalcAppPoolElementsDistribution", as.numeric(scenarioNumberOfCalcAppPoolUsersVariable), as.numeric(scenarioNumberOfCalcAppPoolUsersVariable)))
+   }
+   else if(currentBlock == 2) {   # MEC
+      return(c("mecCalcAppPoolElementsDistribution", as.numeric(mecNumberOfMECPoolElements), as.numeric(mecNumberOfMECPoolElements)))
+   }
+   else {   # Cloud
+      return(c("mecCalcAppPoolElementsDistribution", as.numeric(variable), as.numeric(variable)))
+   }
+}
+
+
+# ###### Number of PUs ######################################################
+mecCalcAppPoolUsersDistribution <- function(currentBlock, totalBlocks,
+                                            variable, gamma, lambda)
+{
+   if((currentBlock < 1) || (currentBlock > totalBlocks) ||
+      (variable < 0.0)) {
+      stop("mecCalcAppPoolUsersDistribution: Check parameters!")
+   }
+   if(totalBlocks != 3) {
+      stop("mecCalcAppPoolUsersDistribution: Invalid setting of totalBlocks!")
+   }
+
+   if(currentBlock == 1) {   # PUs are local-only!
+      return(c("mecCalcAppPoolUsersDistribution", variable, variable))
+   }
+   else {   # No PU in MEC or Cloud!
+      return(c("mecCalcAppPoolUsersDistribution", 0, 0))
+   }
+}
+
+
+# ###### MEC WAN delay distribution #########################################
+mecWANDelayDistribution <- function(currentNetwork, totalNetworks,
+                                    componentType, currentComponent, totalComponents,
+                                    variable, gamma, lambda)
+{
+   if((currentNetwork < 1) || (currentNetwork > totalNetworks) ||
+      (currentComponent < 1) || (currentComponent > totalComponents) ||
+      (variable < 0.0)) {
+      stop("mecWANDelayDistribution: Check parameters!")
+   }
+
+   return(c("MECWANDelayDistribution",
+            as.numeric(variable), as.numeric(variable)))
+}
+
+
+# ###### MEC LAN delay distribution #########################################
+mecLANDelayDistribution <- function(currentNetwork, totalNetworks,
+                                    componentType, currentComponent, totalComponents,
+                                    variable, gamma, lambda)
+{
+   if((currentNetwork < 1) || (currentNetwork > totalNetworks) ||
+      (currentComponent < 1) || (currentComponent > totalComponents) ||
+      (variable < 0.0)) {
+      stop("mecLANDelayDistribution: Check parameters!")
+   }
+
+   if(currentNetwork == 1) {
+      return(c("MECLANDelayDistribution",
+             as.numeric(variable), as.numeric(variable)))
+   }
+   else if(currentNetwork == 2) {
+      r <- runif(1, as.numeric(scenarioNetworkMECMinDelay), as.numeric(scenarioNetworkMECMaxDelay))
+      return(c("MECLANDelayDistribution", r, r))
+   }
+   else {
+      r <- runif(1, as.numeric(scenarioNetworkCloudMinDelay), as.numeric(scenarioNetworkCloudMaxDelay))
+      return(c("MECLANDelayDistribution", r, r))
+   }
+}
+
+
+# ###### MEC capacity distribution ##########################################
+mecCapacityDistribution <- function(currentBlock, totalBlocks,
+                                    currentElement, totalElements,
+                                    variable, gamma, lambda)
+{
+   if((currentBlock < 1) || (currentBlock > totalBlocks) ||
+      (currentElement < 1) || (currentElement > totalElements) ||
+      (variable <= 0.0)) {
+      stop("mecCapacityDistribution: Check parameters!")
+   }
+
+   if(currentBlock == 1) {
+      return(c("MECCapacityDistribution",
+               as.numeric(mecLocalCapacityFactor) * as.numeric(variable),
+               as.numeric(mecLocalCapacityFactor) * as.numeric(variable)))
+   }
+   else if(currentBlock == 2) {
+      return(c("MECCapacityDistribution",
+               as.numeric(mecMECCapacityFactor) * as.numeric(variable),
+               as.numeric(mecMECCapacityFactor) * as.numeric(variable)))
+   }
+   else {
+      return(c("MECCapacityDistribution",
+               as.numeric(variable), as.numeric(variable)))
+   }
+}
+
+
+# ###########################################################################
+
+rspsim5DefaultConfiguration <- append(rspsim5DefaultConfiguration, list(
+   list("mecNumberOfMECPoolElements",   4),
+
+   list("mecLocalDelayVariable",         1.0),
+   list("scenarioNetworkMECMinDelay",    5.0),
+   list("scenarioNetworkMECMaxDelay",   15.0),
+   list("scenarioNetworkCloudMinDelay", 30.0),
+   list("scenarioNetworkCloudMaxDelay", 300.0),
+
+   list("mecLocalCapacityFactor",       0.1),
+   list("mecMECCapacityFactor",         0.5)
+))
+
+simulationConfigurations <- list(
+   # Local + MEC + Cloud
+   list("scenarioNumberOfLANs", 3),
+   list("scenarioNumberOfCalcAppPoolElementsDistribution", "mecCalcAppPoolElementsDistribution"),
+   list("scenarioNumberOfCalcAppPoolUsersDistribution",    "mecCalcAppPoolUsersDistribution"),
+
+   list("scenarioNetworkWANDelayDistribution",             "mecWANDelayDistribution"),
+   list("scenarioNetworkLANDelayDistribution",             "mecLANDelayDistribution"),
+   list("calcAppPoolElementServiceCapacityDistribution",   "mecCapacityDistribution"),
+
+   list("scenarioNumberOfCalcAppPoolElementsVariable",     10),
+   list("mecNumberOfMECPoolElements",                      4),
+
+   list("calcAppPoolElementSelectionPolicy",               "PriorityLeastUsed", "PriorityLeastUsedDPF", "PriorityLeastUsedDegradationDPF"),
+#    "Random", "RoundRobin", "LeastUsed",
+   list("calcAppPoolElementSelectionPolicyLoadDPF",        0.0001),
+   list("calcAppPoolElementSelectionPolicyWeightDPF",      0.0001),
+
+   list("calcAppPoolElementServiceCapacityVariable",       1000000),
+   list("calcAppPoolElementServiceMinCapacityPerJob",      200000),
+   list("mecLocalCapacityFactor",                          0.2),   # !!!!!!!
+   list("mecMECCapacityFactor",                            1.0),   # !!!!!!!
+
+   list("scenarioNumberOfCalcAppPoolUsersVariable",        1, 5, 10, 20, 50, 75, 100),
+   list("calcAppPoolUserServiceJobSizeVariable",           1e5, 5e5, 1e6),
+   list("calcAppPoolUserServiceJobIntervalVariable",       10),
+
+   list("scenarioNetworkLANDelayVariable",                   1.0),   # Local
+   list("scenarioNetworkMECMinDelay",                        5.0),   # MEC (lower bound)
+   list("scenarioNetworkMECMaxDelay",                       15.0),   # MEC (lower bound)
+   list("scenarioNetworkCloudMinDelay",                     30.0),   # Cloud (lower bound)
+   list("scenarioNetworkCloudMaxDelay",                    300.0),   # Cloud (upper bound)
+
+   list("SPECIAL0", "gammaScenario.lan[0].calcAppPoolElementArray[*].calcAppServer.selectionPolicyLoadDegradation = 1.00"),   # Local: 100%
+   list("SPECIAL1", "gammaScenario.lan[1].calcAppPoolElementArray[*].calcAppServer.selectionPolicyLoadDegradation = 0.10"),   # MEC: 10%
+   list("SPECIAL2", "gammaScenario.lan[2].calcAppPoolElementArray[*].calcAppServer.selectionPolicyLoadDegradation = 0.20")    # Cloud: 50%  !!!!!!!!!!!!!
+)
+
+# ###########################################################################
+
+createSimulation(simulationDirectory, simulationConfigurations, rspsim5DefaultConfiguration)
---
+++ toolchain/plot-mec1-test2.R
@@ -0,0 +1,216 @@
+#!/usr/bin/Rscript
+#
+# SELF — Self-contained User Data Preserving Framework
+# Copyright (C) 2021 by Thomas Dreibholz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Contact: dreibh@simula.no
+
+library("anytime")
+library("data.table")
+library("dplyr", warn.conflicts = FALSE)
+library("ggplot2")
+
+
+plotColours <- c("steelblue4", "steelblue1",
+                 "salmon4",    "salmon1",
+                 "seagreen4",  "seagreen1",
+                 "goldenrod3", "goldenrod1",
+                 "gray40",     "gray10",
+                 "cyan4",      "cyan1",
+                 "yellow4",    "yellow1")
+
+
+# ###### Read output file ###################################################
+readResults <- function(directory)
+{
+   dataName <- paste(sep="/", directory, "controller-CalcAppPEGlobalUsedCapacity.data.bz2")
+   print(paste(sep="", "Trying to read ", dataName, " ..."))
+   data <- read.table(pipe(paste(sep="", "bzcat ", dataName)))
+   # names(dataData)[names(dataData) == "data.flow"] <- "FlowID"
+   # names(dataData)[names(dataData) == "data.flow.ReceivedBitRate"] <- "flow.ReceivedBitRate"
+#
+#    data <- rbind(dataData, passiveData)
+
+   # Convert time stamp with anytime() (time is in UTC!):
+   # data$TimeStamp <- anytime(data$TimeStamp, tz="UTC")
+
+   return(data)
+}
+
+
+# ###### Plot results #######################################################
+plotPEUtilisation <- function(name)
+{
+   # ====== Plot as PDF file ================================================
+   calcAppPETotalUsedCapacity <- data.table(loadResults(paste(sep="/", name, "lan.calcAppPoolElementArray.calcAppServer-CalcAppPETotalUsedCapacity.data.bz2")))
+   calcAppPETotalWastedCapacity <- data.table(loadResults(paste(sep="/", name, "lan.calcAppPoolElementArray.calcAppServer-CalcAppPETotalWastedCapacity.data.bz2")))
+   print(sort(colnames(calcAppPETotalUsedCapacity)))
+
+   cairo_pdf(paste(sep="", name, "-Utilisation.pdf"),
+             width=24, height=8, family="Helvetica", pointsize=22)
+
+   title <- ""
+
+   # ====== Calculate utilisation ===========================================
+   utilisation <- data.table(utilisation = 100.0 * calcAppPETotalUsedCapacity$lan.calcAppPoolElementArray.calcAppServer.CalcAppPETotalUsedCapacity /
+                               (calcAppPETotalUsedCapacity$lan.calcAppPoolElementArray.calcAppServer.CalcAppPETotalUsedCapacity + calcAppPETotalWastedCapacity$lan.calcAppPoolElementArray.calcAppServer.CalcAppPETotalWastedCapacity))
+   calcAppPETotalUsedCapacity <- cbind(calcAppPETotalUsedCapacity, utilisation)
+
+   # ====== Use factors =====================================================
+   calcAppPETotalUsedCapacity$lan <- recode_factor(as.factor(calcAppPETotalUsedCapacity$lan),
+                                                   "0" = "UE",
+                                                   "1" = "MEC",
+                                                   "2" = "PMC")
+   calcAppPETotalUsedCapacity$lan.calcAppPoolElementArray <- factor(calcAppPETotalUsedCapacity$lan.calcAppPoolElementArray)
+   calcAppPETotalUsedCapacity$calcAppPoolUserServiceJobIntervalVariable <- factor(calcAppPETotalUsedCapacity$calcAppPoolUserServiceJobIntervalVariable)
+   calcAppPETotalUsedCapacity$calcAppPoolUserServiceJobSizeVariable <- factor(calcAppPETotalUsedCapacity$calcAppPoolUserServiceJobSizeVariable)
+
+
+   plotColours <- c(
+      "red1",  "green4", "green1",  "blue1", "blue4", "purple1", "purple4"
+   )
+
+
+   # ====== Use dplyr to summarise results ==================================
+   summarised <- calcAppPETotalUsedCapacity %>%
+                    group_by(calcAppPoolElementSelectionPolicy,scenarioNumberOfCalcAppPoolUsersVariable,lan,calcAppPoolUserServiceJobSizeVariable) %>%
+                    summarise(#.groups = "keep",   # Keep the grouping as is. Otherwise, it would drop the last one!
+                              MeanCalcAppPEUtilisation = mean(utilisation),
+                              MinCalcAppPEUtilisation  = min(utilisation),
+                              MaxCalcAppPEUtilisation  = max(utilisation),
+                              Q10CalcAppPEUtilisation  = quantile(utilisation, 0.10),
+                              Q90CalcAppPEUtilisation  = quantile(utilisation, 0.90))
+   print(summarised)
+
+
+   # ====== Create plots ====================================================
+   p <- ggplot(summarised,
+               aes(x = scenarioNumberOfCalcAppPoolUsersVariable,
+                   y = MeanCalcAppPEUtilisation)
+            ) +
+         theme(title             = element_text(size=32),
+               plot.title        = element_text(size=20, hjust = 0.5, face="bold"),
+               axis.title        = element_text(size=20, face="bold"),
+               strip.text        = element_text(size=18, face="bold"),
+               axis.text.x       = element_text(size=16, angle=90, face="bold", colour="black"),
+               axis.text.y       = element_text(size=16, angle=90, hjust=0.5, colour="black"),
+               legend.title      = element_blank(),
+               legend.text       = element_text(size=18, face="bold"),
+#                legend.background = element_rect(colour = bgColor,  fill = lgColor, size=1),
+#                panel.background  = element_rect(fill = paste(sep="", "#", colorCU), color=bgColor, size=2),
+               panel.grid.major  = element_line(size=0.5,  linetype="solid", color="lightgray"),
+               panel.grid.minor  = element_line(size=0.25, linetype="solid", color="lightgray")
+               ) +
+         labs(title = title,
+               x     = "Number of Pool User Instances [1]",
+               y     = "Average Utilisation [%]") +
+         facet_grid(lan ~ calcAppPoolElementSelectionPolicy) +
+         geom_line(aes(color = calcAppPoolUserServiceJobSizeVariable), size = 1) +
+         geom_errorbar(aes(ymin = MinCalcAppPEUtilisation, ymax = MaxCalcAppPEUtilisation, color=calcAppPoolUserServiceJobSizeVariable),
+                        size=1.5, width=.5) +
+         geom_errorbar(aes(ymin = Q10CalcAppPEUtilisation, ymax = Q90CalcAppPEUtilisation, color=calcAppPoolUserServiceJobSizeVariable),
+                        size=0.5, width=.25)
+         geom_ribbon(aes(ymin = Q10CalcAppPEUtilisation, ymax = Q90CalcAppPEUtilisation, color=calcAppPoolUserServiceJobSizeVariable),
+                     size=0.01, linetype=2, alpha=0.1)
+
+   print(p)
+
+   dev.off()
+   return(summarised)
+}
+
+
+# ###### Plot results #######################################################
+plotPUHandlingSpeed <- function(name, createPDF = TRUE)
+{
+
+   # ====== Plot as PDF file ================================================
+   systemAverageHandlingSpeed <- data.table(loadResults(paste(sep="/", name, "controller-SystemAverageHandlingSpeed.data.bz2")))
+   print(sort(colnames(systemAverageHandlingSpeed)))
+
+   if(createPDF) {
+      cairo_pdf(paste(sep="", name, "-HandlingSpeed.pdf"),
+               width=24, height=8, family="Helvetica", pointsize=22)
+      title <- ""
+   }
+
+   # ====== Use factors for LAN and LAN.CalcAppPoolElementArray =============
+   systemAverageHandlingSpeed$calcAppPoolUserServiceJobIntervalVariable <- factor(systemAverageHandlingSpeed$calcAppPoolUserServiceJobIntervalVariable)
+   systemAverageHandlingSpeed$calcAppPoolUserServiceJobSizeVariable <- factor(systemAverageHandlingSpeed$calcAppPoolUserServiceJobSizeVariable)
+
+
+   plotColours <- c(
+      "red1",  "green4", "green1",  "blue1", "blue4", "purple1", "purple4"
+   )
+
+
+   # ====== Use dplyr to summarise results ==================================
+   summarised <- systemAverageHandlingSpeed %>%
+                    group_by(calcAppPoolElementSelectionPolicy,scenarioNumberOfCalcAppPoolUsersVariable,calcAppPoolUserServiceJobSizeVariable) %>%
+                    summarise(#.groups = "keep",   # Keep the grouping as is. Otherwise, it would drop the last one!
+                              MeanCalcAppPUHandlingSpeed = mean(controller.SystemAverageHandlingSpeed),
+                              MinCalcAppPUHandlingSpeed  = min(controller.SystemAverageHandlingSpeed),
+                              MaxCalcAppPUHandlingSpeed  = max(controller.SystemAverageHandlingSpeed),
+                              Q10CalcAppPUHandlingSpeed  = quantile(controller.SystemAverageHandlingSpeed, 0.10),
+                              Q90CalcAppPUHandlingSpeed  = quantile(controller.SystemAverageHandlingSpeed, 0.90))
+   print(summarised)
+
+
+   # ====== Create plots ====================================================
+   p <- ggplot(summarised,
+               aes(x = scenarioNumberOfCalcAppPoolUsersVariable,
+                   y = MeanCalcAppPUHandlingSpeed)
+            ) +
+         theme(title             = element_text(size=32),
+               plot.title        = element_text(size=20, hjust = 0.5, face="bold"),
+               axis.title        = element_text(size=20, face="bold"),
+               strip.text        = element_text(size=18, face="bold"),
+               axis.text.x       = element_text(size=16, angle=90, face="bold", colour="black"),
+               axis.text.y       = element_text(size=16, angle=90, hjust=0.5, colour="black"),
+               legend.title      = element_blank(),
+               legend.text       = element_text(size=18, face="bold"),
+#                legend.background = element_rect(colour = bgColor,  fill = lgColor, size=1),
+#                panel.background  = element_rect(fill = paste(sep="", "#", colorCU), color=bgColor, size=2),
+               panel.grid.major  = element_line(size=0.5,  linetype="solid", color="lightgray"),
+               panel.grid.minor  = element_line(size=0.25, linetype="solid", color="lightgray")
+               ) +
+         labs(title = title,
+               x     = "Number of Pool User Instances [1]",
+               y     = "Handling Speed [Calculations/s]") +
+         facet_grid( ~ calcAppPoolElementSelectionPolicy) +
+#          facet_grid( ~ calcAppPoolUserServiceJobSizeVariable) +
+#          facet_wrap(~calcAppPoolElementSelectionPolicy) +
+         geom_line(aes(color = calcAppPoolUserServiceJobSizeVariable), size = 2) +
+         geom_errorbar(aes(ymin = MinCalcAppPUHandlingSpeed, ymax = MaxCalcAppPUHandlingSpeed, color = calcAppPoolUserServiceJobSizeVariable),
+                        size=1.5, width=.5) +
+         geom_errorbar(aes(ymin = Q10CalcAppPUHandlingSpeed, ymax = Q90CalcAppPUHandlingSpeed, color = calcAppPoolUserServiceJobSizeVariable),
+                        size=0.5, width=.25)
+         geom_ribbon(aes(ymin = Q10CalcAppPUHandlingSpeed, ymax = Q90CalcAppPUHandlingSpeed, color = calcAppPoolUserServiceJobSizeVariable),
+                     size=0.01, linetype=2, alpha=0.1)
+
+   print(p)
+
+   if(createPDF) {
+      dev.off()
+   }
+   return(summarised)
+}
+
+
+# ###### Main program #######################################################
+
+data <- plotPEUtilisation("mec1-test2/Results")
+data <- plotPUHandlingSpeed("mec1-test2/Results")
