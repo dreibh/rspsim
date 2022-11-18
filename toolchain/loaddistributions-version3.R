0c4e2661a9dfec5fbb7f6f2630465538142ace05
Thomas Dreibholz <dreibh@simula.no>
2022-11-18 09:43:41 +0100

    Merged tools updates.

--- toolchain/loaddistributions-version3.R
+++ toolchain/loaddistributions-version3.R
@@ -1,6 +1,6 @@
 # ###########################################################################
 #             Thomas Dreibholz's R Simulation Scripts Collection
-#                 Copyright (C) 2005-2012 by Thomas Dreibholz
+#                 Copyright (C) 2005-2022 by Thomas Dreibholz
 #
 #               Author: Thomas Dreibholz, dreibh@iem.uni-due.de
 # ###########################################################################
