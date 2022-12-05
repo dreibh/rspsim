53ecd30983a8e144bf143313ae2c0ef543fd37d6
Thomas Dreibholz <dreibh@simula.no>
2022-12-05 16:18:24 +0100

    Minor update.

--- toolchain/mec2-baseline.R
+++ toolchain/mec2-baseline.R
@@ -9,7 +9,7 @@ source("simulate-version14.R")
 # ====== Simulation Settings ================================================
 simulationDirectory <- "mec2-baseline"
 simulationRuns <- 3
-simulationDuration <- 7*24*60   # 1 week
+simulationDuration <- 7*24*60 - 21   # 1 week - 21 min
 simulationStoreVectors <- FALSE
 simulationExecuteMake <- TRUE
 simulationScriptOutputVerbosity <- 3
